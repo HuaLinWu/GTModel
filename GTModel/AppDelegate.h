5c3b28cfff39a181d91e058e332cda3b3a263d3a
wuhualin <wuhualin@finogeeks.com>
2018-06-09 15:30:37 +0800

    Initial Commit

---
+++ GTModel/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GTModel
+//
+//  Created by 吴华林 on 2018/6/9.
+//  Copyright © 2018年 吴华林. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ GTModel/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  GTModel
+//
+//  Created by 吴华林 on 2018/6/9.
+//  Copyright © 2018年 吴华林. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
